6147f90a5e1456f024a7527bd2ecaaec4b5c783f
Stevo Bailey <stevo.bailey@eecs.berkeley.edu>
2016-12-18 23:57:27 -0800

    [stevo]: new framework, but testchipip and riscv-tools diverged so bump riscv-fesvr to build toolchain

--- build.sbt
+++ build.sbt
@@ -5,3 +5,11 @@ version := "1.0"
 name := "craft2-chip"
 
 scalaVersion := "2.11.7"
+
+libraryDependencies += "org.spire-math" %% "spire" % "0.11.0"
+
+libraryDependencies += "org.scalanlp" %% "breeze" % "0.12"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "2.2.5",
+  "org.scalacheck" %% "scalacheck" % "1.12.4")
--- generate-pkg-mk.sh
+++ generate-pkg-mk.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 base_dir=$(dirname $0)
+lib_dir=$(echo $1)
+shift
 
 for pkg in $@
 do
     pkg_dir="${base_dir}/${pkg}"
     cat <<MAKE
-${base_dir}/lib/$(basename ${pkg}).stamp: \$(call lookup_scala_srcs, ${pkg_dir}) \$(rocketchip_stamp)
-	mkdir -p ${base_dir}/lib
+${lib_dir}/$(basename ${pkg}).stamp: \$(call lookup_scala_srcs, ${pkg_dir}) \$(rocketchip_stamp)
+	mkdir -p ${lib_dir}
 	rm -f ${pkg_dir}/lib
-	ln -s ${base_dir}/lib ${pkg_dir}/lib
-	cd ${pkg_dir} && \$(SBT) package
-	cp ${pkg_dir}/target/scala-2.11/*.jar ${base_dir}/lib
+	ln -s ${lib_dir} ${pkg_dir}/lib
+	cd ${pkg_dir} && \$(SBT) publish-local
+	cp ${pkg_dir}/target/scala-2.11/*.jar ${lib_dir}
 	touch \$@
 MAKE
 done
