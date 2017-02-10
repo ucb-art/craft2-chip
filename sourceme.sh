ababc0282807ef658ccec26d68cf5843d252e296
Stevo Bailey <stevo.bailey@eecs.berkeley.edu>
2017-02-10 13:56:05 -0800

    [stevo]: bump and work on ipxact configs

--- sourceme.sh
+++ sourceme.sh
@@ -9,3 +9,12 @@ export LD_LIBRARY_PATH=$RISCV/lib64:$RISCV/lib:$LD_LIBRARY_PATH
 export PATH=/tools/synopsys/vcs/G-2012.09/bin:$PATH
 export VCS_HOME=/tools/synopsys/vcs/G-2012.09/
 export VCS_64=1
+
+# memory compiler
+export INTERRAD_LICENSE_FILE=/tools/commercial/interra/flexlm/license_N28.dat
+export TSMCHOME=/tools/tstech16/CLN16FFC/TSMCHOME
+
+# temporary, hopefully
+export MC2_INSTALL_DIR=~stevo.bailey/mc2/MC2_2013.12.00.f
+
+export PATH=$PATH:$MC2_INSTALL_DIR/bin
---
+++ vsrc/SimSerial.v
@@ -0,0 +1,1 @@
+../dsp-framework/testchipip/vsrc/SimSerial.v
