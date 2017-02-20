83bd0db22f2086d94a0e0b34250200ccce243088
Stevo Bailey <stevo.bailey@eecs.berkeley.edu>
2017-02-20 00:19:19 -0800

    [stevo]: add tuner and filter, other minor things

--- generate-pkg-mk.sh
+++ generate-pkg-mk.sh
@@ -10,7 +10,7 @@ do
     cat <<MAKE
 ${lib_dir}/$(basename ${pkg}).stamp: \$(call lookup_scala_srcs, ${pkg_dir}) \$(rocketchip_stamp)
 	mkdir -p ${lib_dir}
-	rm -f ${pkg_dir}/lib
+	rm -rf ${pkg_dir}/lib
 	ln -s ${base_dir}/lib ${pkg_dir}/lib
 	cd ${pkg_dir} && \$(SBT) publish-local
 	touch \$@
