55c82ec8d32395022a09aa7c3f79ad5d2fa2c3a0
Paul Rigge <rigge@berkeley.edu>
2017-02-27 21:01:12 -0800

    Pull ADC IOs to top level

--- src/main/scala/craft/Top.scala
+++ src/main/scala/craft/Top.scala
@@ -15,12 +15,7 @@ class ExampleTopWithCraft2DSP(q: Parameters) extends ExampleTop(q)
     new ExampleTopWithCraft2DSPModule(p, this, new CraftTopBundle(p)))
 }
 
-class CraftTopBundle(p: Parameters) extends ExampleTopBundle(p) {
-  val firstBlockId = p(DspChainKey(p(DspChainId))).blocks.head._2
-  val firstBlockWidth = p(GenKey(firstBlockId)).genIn.getWidth * p(GenKey(firstBlockId)).lanesIn
-  val stream_in = Flipped(ValidWithSync(UInt( firstBlockWidth.W )))
-  val dsp_clock = Flipped(Bool())
-}
+class CraftTopBundle(p: Parameters) extends ExampleTopBundle(p) with ADCTopLevelIO
 
 class ExampleTopWithCraft2DSPModule(p: Parameters, l: ExampleTopWithCraft2DSP, b: CraftTopBundle)
   extends ExampleTopModule(p, l, b) with PeripheryCraft2DSPModule
